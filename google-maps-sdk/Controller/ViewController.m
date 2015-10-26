37b428490419f9534007dacd90187d7d4a55ba12
Treinamento Mobile <bruno.sapienza@gmail.com>
2015-10-26 11:00:41 -0200

    first commit

---
+++ google-maps-sdk/Controller/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  google-maps-sdk
+//
+//  Created by Treinamento Mobile on 10/26/15.
+//  Copyright © 2015 Treinamento Mobile. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <GoogleMaps/GoogleMaps.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude: -23.641674
+                                                            longitude: -46.728921
+                                                                  zoom: 16];
+                                 
+    GMSMapView *mapView = [GMSMapView mapWithFrame: self.view.bounds camera: camera];
+    
+    [self.view addSubview:mapView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
